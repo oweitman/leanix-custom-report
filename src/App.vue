63cdb7b047d3ad164d58658aaa05397e008b9a2b
oliver.weitmann@detecon.com <oliver.weitmann@detecon.com>
2023-08-02 14:47:17 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- we'll use this template tag for declaring our custom report html -->
+  <div>Hi from LeanIX Custom Report</div>
+  <div>{{ state }}</div>  
+</template>
+
+<script setup>
+  // all the state variables and business logic will be declared here
+  import '@leanix/reporting';
+  import { ref } from 'vue'
+  
+  var state = ref('empty');
+
+lx.init()
+.then(function (setupInfo) {
+  debugger;
+  state = JSON.stringify(setupInfo.savedState?.customState);  
+  var config = {
+    };
+    lx.publishState({"test":"test"});
+    lx.ready(config);  
+});  
+</script>
